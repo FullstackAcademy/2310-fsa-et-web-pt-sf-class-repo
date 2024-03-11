9bce687ceb67e1328f1c9ba06d30ebe44b3e8ab4
Eliot Szwajkowski <eliotpszw@gmail.com>
2024-03-11 19:06:33 -0400

    live merge

--- reacto/merge_sort.js
+++ reacto/merge_sort.js
@@ -1,34 +1,50 @@
-function merge(left, right) {
-    let arr = [];
+const merge = (left, right) => {
+    const sortedArr = [];
 
-    // Break out of loop if any one of the array gets empty
     while (left.length && right.length) {
-
-        // Pick the smaller among the smallest element of left and right arrays
-        if (left[0] < right[0]) {
-            arr.push(left.shift());
-        }
-        else {
-            arr.push(right.shift());
+        if (left[0] <= right[0]) {
+            sortedArr.push(left.shift());
+        } else {
+            sortedArr.push(right.shift());
         }
     }
 
-    // Concatenating the leftover elements
-    return [...arr, ...left, ...right];
-}
+    const combinedArr = [...sortedArr, ...left, ...right];
 
-function mergeSort(array) {
-    const half = Math.round(array.length / 2);
+    console.log('Combined: ', combinedArr);
 
-    // Base case or stopping condition
-    if (array.length < 2) {
-        return array;
+    return combinedArr;
+};
+
+// MS [3, 2, 4, 1]
+// MS [3, 2]
+// MS [3]
+// MS [2]
+// M ([3], [2]) => [2, 3]
+// MS [4, 1]
+// MS [4]
+// MS [1]
+// M ([4], [1]) => [1, 4]
+// M ([2, 3], [1, 4]) => [1, 2, 3, 4]
+const mergeSort = (listOfNumbers) => {
+    if (listOfNumbers.length < 2) {
+        return listOfNumbers;
     }
 
-    const left = array.splice(0, half); // array of first half
-    const right = array; // array of second half
+    const halfPoint = Math.round(listOfNumbers.length / 2);
 
-    return merge(mergeSort(left), mergeSort(right)); // Recursive call
-}
+    const left = listOfNumbers.splice(0, halfPoint);
+    const right = listOfNumbers;
 
-console.log(mergeSort([3, 1, 7, -2, 15, 0, 100, 2]));
+    console.log('Left: ', left, 'Right: ', right);
+
+    return merge(mergeSort(left), mergeSort(right));
+};
+
+const generateArray = (size) => {
+    return new Array(size).fill('').map(() => {
+        return Math.round(Math.random() * 1000);
+    });
+};
+
+console.log('Final Result: ', mergeSort(generateArray(25)));
